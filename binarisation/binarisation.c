4fa6826807900ea900666c8c4e33741fcee03655
Geoffroy du Mesnil du Buisson <geoffroydumesnildubuisson@MacBook-Air-de-Geoffroy.local>
2020-10-25 14:40:30 +0100

    test RLSA function. DO NOT TOUCH THE binarisation FILE git add -A git push q :q

--- binarisation/binarisation.c
+++ binarisation/binarisation.c
@@ -169,7 +169,7 @@ Matrix imgToMat(SDL_Surface* img){
            Uint32 pixel1 = SDL_MapRGB(img->format, 255, 255, 255);
            put_pixel(img,i,j,pixel1);
 
-           derhistoMat.matrix[i*derhistoMat.width+j] = 0; 
+           derhistoMat.matrix[i*derhistoMat.width+j] = 0;
 
          }
          else{
@@ -220,6 +220,180 @@ int matToImg(Matrix M, char *str){//geoffroy
   SDL_FreeSurface(img);
 
   return 0;
+}
+
+void print_Matrix(Matrix M)    // Print a matrix
+{
+  int n = 0;
+    for (int i = 0 ; i < M.height ; i++)
+        {
+                for (int j = 0 ; j < M.width ; j++){
+                  printf("%i  ,", (int)M.matrix[i*M.width+j]);
+                  n++;
+                }
+                printf("\n");
+        }
+
+    printf("\n");
+    printf("%i\n", n);
+}
+
+double randfrom(double min, double max)    // Return a randmon double between min and max
+{
+    double range = (max - min);
+    double div = RAND_MAX / range;
+    return min + (rand() / div);
+}
+
+Matrix new_alea_Matrix(int H, int W)    // Create a new matrix init with random double
+{
+    Matrix M;
+
+        M.height = H;
+        M.width = W;
+
+        M.matrix = malloc(H*W*sizeof(double));
+        for (int i = 0 ; i < H*W ; i++)
+        {
+                M.matrix[i] = rand() % 2;
+        }
+
+        return M;
+
+}
+
+Matrix horizonRLSA (Matrix img, int seuil){
+  /*
+  description :
+  function that go thru a matrix from horizontal way
+  and apply to it the RLSA algorthm
+
+  parameters :
+  Matrix img : matrix where we are going to apply the RLSA algorthm
+  int seuil : limite the we fix for the RLSA system
+
+
+  dates/authors :
+  14/10
+  geoffroy du mesnil du buisson
+
+  */
+
+
+  Matrix m = copyMatrix(img);
+
+  int nbzeros;//nubers of 0 followed
+
+  int j;//index on the width of the matrix
+  int k;//index on the nubers of 0 followed
+
+  for (int i = 0; i < m.height ; i++) {//classic search in the matrix
+
+    j=0;
+    while (j < m.width) {
+
+      if ((int)m.matrix[i*m.width+j]!=1){
+
+        k=j;
+        nbzeros=1;
+
+        while (((int)m.matrix[i*m.width+k]!=1)&&(k< m.width)) {//counting the numbers of 0 followed between two 1
+
+          nbzeros++;
+          k++;
+        }
+        if (nbzeros<=seuil){//if the numbers of 0 is lower or equal to the limite (seuil) all the 0 pass to 1
+
+          for (int l = j; l < nbzeros; l++) {
+
+            m.matrix[i*m.width+l]=1;
+          }
+        }
+        j+=nbzeros;//we increment the index of the number of 0 we had seen
+      }//end if
+      j++;
+    }//end while
+  }
+  return m;//return the matrix
+}
+
+Matrix verticRLSA (Matrix img, int seuil){
+  /*
+  description :
+  function that go thru a matrix from vertical way
+  and apply to it the RLSA algorthm
+
+  parameters :
+  Matrix img : matrix where we are going to apply the RLSA algorthm
+  int seuil : limite the we fix for the RLSA system
+
+
+  dates/authors :
+  14/10
+  geoffroy du mesnil du buisson
+
+  */
+
+  Matrix m = copyMatrix(img);
+
+  int nbzeros;//nubers of 0 followed
+
+  int i;//index on the height of the matrix
+  int k;//index on the nubers of 0 followed
+  for (int j = 0; j < m.width ; j++) {//vertical search in the matrix
+
+    i=0;
+    while (i < m.height) {
+
+      if ((int)m.matrix[i*m.width+j]!=1){
+
+        k=i;
+        nbzeros=0;
+        while ((int)m.matrix[i*m.width+k]!=1&&k< m.height) {//counting the numbers of 0 followed between two 1
+
+          nbzeros++;
+          k++;
+        }
+        if (nbzeros<=seuil){//if the numbers of 0 is lower or equal to the limite (seuil) all the 0 pass to 1
+
+          for (int l = i; l < nbzeros; l++) {
+
+            m.matrix[i*m.width+l]=1;
+          }
+        }
+        i+=nbzeros+1;//we increment the index of the number of 0 we had seen
+      }//end if
+      i++;
+    }//end while
+  }
+  return m;//return the matrix
+}
+
+Matrix rlsa(Matrix img, int verticSeuil, int horizonSeuil){
+  /*
+  description :
+  return the mix of horizonRLSA and verticRLSA in a new matrix m
+
+  parameters :
+  Matrix img : the matrix of the whole image only compose with somme 0 and 1
+  int verticSeuil : the limite of zero we want for the RLSA in vertical
+  int horizonSeuil : the limite of zero we want for the RLSA in horizontal
+
+  dates/authors :
+  14/10
+  geoffroy du mesnil du buisson
+
+  */
+
+  Matrix horizonMatrix = horizonRLSA(img, horizonSeuil);
+  Matrix verticMatrix = verticRLSA(img, verticSeuil);
+
+  Matrix m = andLogicMartix(horizonMatrix,verticMatrix);
+
+  freeMatrix(horizonMatrix);
+  freeMatrix(verticMatrix);
+
+  return m;//return the new matrix
 }
 
 int main()
@@ -304,7 +478,7 @@ int main()
 		         Uint32 pixel1 = SDL_MapRGB(image_surface->format, 255, 255, 255);
 		         put_pixel(image_surface,i,j,pixel1);
 
-             derhistoMat.matrix[i*derhistoMat.width+j] = 0;
+             derhistoMat.matrix[i*derhistoMat.width+j] = 1;
 
 	         }
 	         else{
@@ -312,7 +486,7 @@ int main()
 		         Uint32 pixel1 = SDL_MapRGB(image_surface->format, 0, 0, 0);
 		         put_pixel(image_surface,i,j,pixel1);
 
-		         derhistoMat.matrix[i*derhistoMat.width+j] = 1;
+		         derhistoMat.matrix[i*derhistoMat.width+j] = 0;
 	         }
 	      }
     }
@@ -323,6 +497,31 @@ int main()
     update_surface(screen_surface, image_surface);
 
     wait_for_keypressed();
+
+    print_Matrix(derhistoMat);
+
+
+
+    //Matrix m = rlsa(rlsa(derhistoMat,1000000000,1000000000),1000000000,1000000000);
+    //Matrix m = rlsa(derhistoMat,30,30);
+
+    Matrix m = new_alea_Matrix(20, 20);
+
+    print_Matrix(m);
+
+
+    m = horizonRLSA(m,2);
+
+    //m= rlsa(m,2,2);
+
+    print_Matrix(m);
+
+    //matToImg(m, "rlsatest2_1_img");
+
+    //update_surface(screen_surface, image_surface);
+
+
+    //wait_for_keypressed();
 
     SDL_FreeSurface(image_surface);
 
