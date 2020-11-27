0dc997ef18a4ef4d2cbc3b49de416d56f33b5eb3
Anna Barry <anna.barry@epita.fr>
2020-11-27 23:29:14 +0100

    rot manual working but not interpolation

--- pré-traitement/Rotations/rotationMAN.c
+++ pré-traitement/Rotations/rotationMAN.c
@@ -19,7 +19,7 @@
  *
  *      Surface corresponding to picture after otsu method
  *
- *  Version 0.3 on 15/11/2020
+ *  Version 1.2 on 27/11/2020
  */
 
 
@@ -27,7 +27,16 @@
 // 1- Initialisation of variables
 //
 // Rotation is counter-clockwise here so degrees 0 - 360 are negative
+double getNewX(int x,int y,double sinA,double cosA, int xori,int yori)
+{
+    return ((-sinA * (y- yori))+ (cosA*(x-xori)))+ xori;
+}
 
+int getNewY(int x,int y,double sinA,double cosA, int xori,int yori)
+{
+    return ((cosA*(y-yori))+(sinA*(x-xori)))+yori;
+}
+#define max(a,b) (a>=b?a:b)
 #define Pi 3.141592653589
 SDL_Surface* ManualRotation(SDL_Surface* image_surface , int degrees)
 {
@@ -41,112 +50,69 @@ SDL_Surface* ManualRotation(SDL_Surface* image_surface , int degrees)
     double xO=0;
     double yO=0;
 
-    // New surface
-    SDL_Surface* result= image_surface;
-
     int OriW = image_surface->w;
     int OriH = image_surface->h;
+
+    //New height and width Range
+    double topss=max(getNewX(0,0, sinA, cosA, OriW/2, OriH/2),getNewX((OriW-1),0,sinA, cosA, OriW/2, OriH/2));
+    double downss=max(getNewX(0,(OriH-1),sinA, cosA, OriW/2, OriH/2),getNewX((OriW-1),(OriH-1),sinA, cosA, OriW/2, OriH/2));
+
+    double NewW1=max(topss,downss);
+
+    int NewW= (int)(NewW1);
+
+    double t2=max(getNewY(0,0,sinA, cosA, OriW/2, OriH/2),getNewY((OriW-1),0,sinA, cosA, OriW/2, OriH/2));
+    double d2=max(getNewY(0,(OriH-1),sinA, cosA, OriW/2, OriH/2),getNewY((OriW-1),(OriH-1),sinA, cosA, OriW/2, OriH/2));
+
+    double NewH2=max(t2,d2);
+
+    int NewH=(int)(NewH2);
+    // New surface
+    SDL_Surface* result=NULL;
+    result=SDL_CreateRGBSurface (0, NewW, NewH, 32, 0, 0, 0, 0);
+
     // Initialize with black pixels
-    for (int i = 0; i < OriH; ++i)
+    for (int i = 0; i < NewH; ++i)
     {
-        for (int j = 0; j < OriW; ++j)
+        for (int j = 0; j < NewW; ++j)
         {
             // initialise with black pixels
-            Uint32 pixel = SDL_MapRGB(image_surface->format, 0, 0, 0);
+            Uint32 pixel = SDL_MapRGB(result->format, 0, 0, 0);
             put_pixel(result,j,i,pixel);
         }
     }
 
-//New height and width Range
-    int NewW= OriW*fabs(sinA)+OriH*fabs(cosA);
-    int NewH= OriW*fabs(cosA)+OriH*fabs(sinA);
 
-    //Offset (x=ay+b where b is the offset)
-    // Temp Width and Height
-    int temp=sqrt((double)OriW * OriW + (double)OriH *OriH) + 10;
+    printf("Original surface = %i x %i and new Surface= %i x %i \n",OriW,OriH,NewW,NewH);
+    printf("SinA is %f and CosA= %f \n \n",sinA, cosA);
 
-    int xOff=temp/2;
-    int yOff=temp/2;
-
-    //Max and Min original Pixels to rotate
-    int yMin=(temp-NewH)/2.;
-    int yMax=yMin+NewH+1;
-    int xMin=(temp-NewW)/2.;
-    int xMax=xMin+NewW +1;
-
-for(y=yMin; y<=yMax; y++)
+    for(y=0; y<= OriH; y++)
     {
-        for(x=xMin; x<=xMax; x++)
+        for(x=0; x<=OriW; x++)
         {
-            //Getting Original Coordinates
-            yO=sinA*(double)(x-xOff)+cosA*(double)(y-yOff);
-            yO+=(double)((int)(OriH/2));
-            xO=cosA*(double)(x-xOff)-sinA*(double)(y-yOff);
-            xO+=(double)((int)(OriW/2));
 
-            //If in original range -> if not just black
-            if (yO>=0. && yO<OriH-0.5 && xO>=0. && xO<OriW-0.5)
+             if(x==OriW/2 && y==OriH/2)
             {
-                    //Bilinear Interpolation
-                    int xS=floor(xO);
-                    int xL=ceil(xO);
-                    int yS= floor(yO);
-                    int yL= ceil(yO);
-
-                    int fDeltaX=xO- (double)xS;
-                    int fDeltaY=yO- (double)yS;
-                    // Pixel values around pixel(i,j) xS= TopLeft xL=TopRight
-                    // yS= Bottom Left yL BottomRight
-                    Uint32 topLeft= get_pixel(image_surface,xS,yS);
-                    Uint32 topRight= get_pixel(image_surface,xL,yS);
-                    Uint32 botLeft= get_pixel(image_surface,xS,yL);
-
-                    double fTopRed = (1 - fDeltaX) * rclrTopLeft + fDeltaX * rTR;
-                    double fTopGreen = (1 - fDeltaX) * gTopLeft + fDeltaX * gTR;
-                    double fTopBlue = (1 - fDeltaX) * bTopLeft + fDeltaX * bTR;
-
-                    // Horizontal linear interpolation between bottom neighbours
-                    Uint8 rBL, gBL, bBL;
-                    SDL_GetRGB(botLeft, image_surface->format, &rBL, &gBL, &bBL);
-
-                    Uint8 rBR, gBR, bBR;
-                    SDL_GetRGB(botRight, image_surface->format, &rBR, &gBR, &bBR);
-
-double fTopRed = (1 - fDeltaX) * rclrTopLeft + fDeltaX * rTR;
-                    double fTopGreen = (1 - fDeltaX) * gTopLeft + fDeltaX * gTR;
-                    double fTopBlue = (1 - fDeltaX) * bTopLeft + fDeltaX * bTR;
-
-                    // Horizontal linear interpolation between bottom neighbours
-                    Uint8 rBL, gBL, bBL;
-                    SDL_GetRGB(botLeft, image_surface->format, &rBL, &gBL, &bBL);
-
-                    Uint8 rBR, gBR, bBR;
-                    SDL_GetRGB(botRight, image_surface->format, &rBR, &gBR, &bBR);
-
-
-                    double fBottomRed = (1 - fDeltaX) * rBL + fDeltaX * rBR;
-                    double fBottomGreen = (1 - fDeltaX) * gBL + fDeltaX * gBR;
-                    double fBottomBlue = (1 - fDeltaX) * bBL + fDeltaX * bBR;
-
-                    //Vertical linear interpolation between top and bottom results
-                    int iRed = (int)(round((1 - fDeltaY) * fTopRed + fDeltaY * fBottomRed));
-                    int iGreen = (int)(round((1 - fDeltaY) * fTopGreen + fDeltaY * fBottomGreen));
-                    int iBlue = (int)(round((1 - fDeltaY) * fTopBlue + fDeltaY * fBottomBlue));
-
-                    //Valid Colours
-                    if (iRed < 0) iRed = 0;
-                    if (iRed > 255) iRed = 255;
-                    if (iGreen < 0) iGreen = 0;
-                    if (iGreen > 255) iGreen = 255;
-                    if (iBlue < 0) iBlue = 0;
-                    if (iBlue > 255) iBlue = 255;
-
-
-                    put_pixel(result,y, x,SDL_MapRGB(image_surface->format, iRed, iGreen, iBlue));
+                put_pixel(result,x,y,get_pixel(image_surface,x,y));
+                continue;
             }
+
+
+
+            xO=getNewX(x,y, sinA, cosA, OriW/2, OriH/2);
+            yO=getNewY(x,y, sinA, cosA, OriW/2, OriH/2);
+            if (yO>0 && yO<NewH && xO>0 && xO<NewW)
+            {
+
+                //ORIGINAL COLOURS
+                put_pixel(result,xO,yO,get_pixel(image_surface,x,y));
+
+            }
+            
 
         }
     }
 
     return result;
+
 }
