ab74288dce30b49abc2dd30dbeeebe88afca5e4d
MATURANA <marie@MacBook-Pro-de-MATURANA.local>
2020-12-03 22:20:02 +0100

    binarisation = nouveau code pb de malloc et memoire + debut interface editeur texte

--- Tools/matrix.h
+++ Tools/matrix.h
@@ -11,6 +11,10 @@ typedef struct Matrix{
 
 Matrix newMatrix(int H, int W);
 
+double matrix_get(Matrix m, int w, int h);
+
+void matrix_put(Matrix m, size_t w, size_t h, double value);
+
 Matrix andLogicMartix(Matrix m1,Matrix m2);
 
 Matrix cutMatrix(Matrix M, int x ,int y,int height,int width);
--- pré-traitement/binarisation/binarisation.h
+++ pré-traitement/binarisation/binarisation.h
@@ -1,15 +1,19 @@
 #ifndef BINARISATION_H
 #define BINARISATION_H
 
+//otsu threshold calculation function
+Matrix matrix_grayscale_to_binar(Matrix M, int threshold); 
 
-void init_sdl();
-SDL_Surface* load_image(char *path);
-SDL_Surface* display_image(SDL_Surface *img);
-void wait_for_keypressed();
-void SDL_FreeSurface(SDL_Surface *surface);
-float compteOtsuseuil(float hist[],int width, int height );
+Matrix surface_to_matrix_grayscale(SDL_Surface *img);
+
+Matrix create_histogram(Matrix M);
+
+int max(int a, int b);
+
+int histogram_max(Matrix histogram);
+
+int otsu(Matrix m);
+
 int matToImg(Matrix M, char *str);
-
-
 
 #endif
