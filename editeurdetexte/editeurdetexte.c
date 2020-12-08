e48602da8871f61e6bae2bbcc1ba022ac1707f40
mariemtrn <marie.maturana@epita.fr>
2020-12-09 00:57:41 +0100

    techniques de presentation avancées

--- editeurdetexte/editeurdetexte.c
+++ editeurdetexte/editeurdetexte.c
@@ -10,26 +10,29 @@ int main (int argc, char **argv)
 {
   GtkWidget *p_window = NULL;
   GtkWidget *p_main_box = NULL;
-  GtkWidget *p_button_box = NULL;
+  GtkWidget *p_table = NULL;;
   GtkWidget *p_text_view = NULL;
   GtkWidget* Label = NULL;
-  gchar* Texte = NULL;
+  gchar* Texte = NULL;  
 
-  /* Initialisation de GTK+ */
+  // Initialization of gtk
   gtk_init (&argc, &argv);
 
-  /* Creation de la fenetre principale de notre application */
+  // Create the first window 
   p_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_fullscreen(GTK_WINDOW (p_window));
   gtk_window_maximize (GTK_WINDOW (p_window));
   gtk_window_set_title (GTK_WINDOW (p_window), "INTERFACE");
   g_signal_connect (G_OBJECT (p_window), "destroy", G_CALLBACK (cb_quit), NULL);
 
-  /* Creation du conteneur principal */
+  
+  //create the box for have numerous gtkwidget
   p_main_box = gtk_vbox_new (FALSE, 0);
   gtk_container_add (GTK_CONTAINER (p_window), p_main_box);
 
-  Texte = g_locale_to_utf8("<span face=\"Verdana\" foreground=\"#000000\" size=\"xx-large\"><b>Optical Character Recognition</b></span>\n <span face=\"Verdana\" foreground=\"#808080\" size=\"large\">By Anna Barry, Goeffroy Du Mesnil Du Buisson, Marie Maturana, Marine Thunet</span>\n", -1, NULL, NULL, NULL);  //Convertion du texte avec les balises
+  //create the texte
+
+  Texte = g_locale_to_utf8("<span face=\"Times New Roman\" foreground=\"#7A81FF\" size=\"xx-large\" style=\"italic\"><b>Optical Character Recognition</b></span>\n <span face=\"Verdana\" foreground=\"#000000\" size=\"large\">By Anna Barry, Goeffroy Du Mesnil Du Buisson, Marie Maturana, Marine Thunet</span>\n", -1, NULL, NULL, NULL);  //Convertion du texte avec les balises
     Label=gtk_label_new(Texte); // Application de la convertion à notre label
     
     g_free(Texte); // Libération de la mémoire
@@ -39,30 +42,54 @@ int main (int argc, char **argv)
     gtk_label_set_justify(GTK_LABEL(Label), GTK_JUSTIFY_CENTER); // On centre notre texte
 
     gtk_container_add(GTK_CONTAINER(p_main_box), Label);  // On ajoute le label a l'interieur de 'Fenetre'
-
+  
  
-  /* Creation du conteneur pour les boutons */
-  p_button_box = gtk_hbutton_box_new ();
-  gtk_box_pack_start (GTK_BOX (p_main_box), p_button_box, FALSE, FALSE, 0);
 
-  /* Creation du bouton "Ouvrir" */
-  {
-    GtkWidget *p_button = NULL;
+    //create the button box
+    p_table = gtk_table_new(2, 3, FALSE);
+    //gtk_table_set_col_spacing (p_table, 1, 4); 
+    gtk_container_add (GTK_CONTAINER(p_main_box), p_table);
+    
+      
+    //insert image
+    {
+      GtkWidget *p_image = NULL;
+      p_image = gtk_image_new_from_file("intro.png");
+      gtk_table_attach(GTK_TABLE(p_table), p_image,  1, 2, 0, 1, GTK_EXPAND, GTK_EXPAND, 10, 10);
+    }
+    
+    //create the start button
+    {
+      GtkWidget *p_button = NULL;
 
-    p_button = gtk_button_new_with_label("OPEN");
-    g_signal_connect (G_OBJECT (p_button), "clicked", G_CALLBACK (cb_openpage2), p_text_view);
-    gtk_box_pack_start (GTK_BOX (p_button_box), p_button, FALSE, FALSE, 0);
-  }
+      p_button = gtk_button_new_with_label("INFORMATIONS");
+      g_signal_connect (G_OBJECT (p_button), "clicked", G_CALLBACK (cb_openpage2), p_text_view);
+      gtk_widget_set_size_request(p_button, 250, 200);
+      gtk_table_attach(GTK_TABLE(p_table), p_button, 0, 1, 1, 2, GTK_EXPAND, GTK_EXPAND, 10, 10);
+    }
 
-  /* Creation du bouton "Quitter" */
-  {
-    GtkWidget *p_button = NULL;
+    //create the start button
+    {
+      GtkWidget *p_button = NULL;
 
-    p_button = gtk_button_new_with_label("QUIT");
-    g_signal_connect (G_OBJECT (p_button), "clicked", G_CALLBACK (cb_quit), NULL);
-    gtk_box_pack_start (GTK_BOX (p_button_box), p_button, FALSE, FALSE, 0);
-  }
+      p_button = gtk_button_new_with_label("START");
+      g_signal_connect (G_OBJECT (p_button), "clicked", G_CALLBACK (cb_openpage2), p_text_view);
+      gtk_widget_set_size_request(p_button, 250, 200);
+      gtk_table_attach(GTK_TABLE(p_table), p_button, 1, 2, 1, 2, GTK_EXPAND, GTK_EXPAND, 10, 10);
+    }
 
+    /* Creation du bouton "Quitter" */
+    {
+      GtkWidget *p_button = NULL;
+
+      p_button = gtk_button_new_with_label("QUIT");
+      g_signal_connect (G_OBJECT (p_button), "clicked", G_CALLBACK (cb_quit), NULL);
+      gtk_widget_set_size_request(p_button, 250, 200);
+      gtk_table_attach(GTK_TABLE(p_table), p_button, 2, 3, 1, 2, GTK_EXPAND, GTK_EXPAND, 10, 10);
+    }
+
+
+  
   /* Affichage de la fenetre principale */
   gtk_widget_show_all (p_window);
   /* Lancement de la boucle principale */
