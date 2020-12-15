78842cbc78645556f035ca37af9c50d00f46d16f
Marine Thunet <marinethunet@mbp-de-marine.lan>
2020-12-15 03:39:46 +0100

    images pour marie

--- pré-traitement/Rotations/rotationMAN.d
+++
@@ -1,4 +0,0 @@
-../../pré-traitement/Rotations/rotationMAN.o: \
- ../../pré-traitement/Rotations/rotationMAN.c \
- ../../pré-traitement/Rotations/../../Tools/pixel_operations.h \
- /usr/include/SDL/SDL.h ../../pré-traitement/Rotations/rotationMAN.h
--- réseau_de_neurones/CNN/RecogniseASCII.d
+++
@@ -1,9 +0,0 @@
-../../réseau_de_neurones/CNN/RecogniseASCII.o: \
- ../../réseau_de_neurones/CNN/RecogniseASCII.c \
- ../../réseau_de_neurones/CNN/LeNet.h \
- ../../réseau_de_neurones/CNN/../../Tools/matrix.h \
- ../../réseau_de_neurones/CNN/../../Tools/pixel_operations.h \
- /usr/include/SDL/SDL.h \
- ../../réseau_de_neurones/CNN/../../Tools/image_operations.h \
- ../../réseau_de_neurones/CNN/../../pré-traitement/binarisation/binarisation.h \
- /usr/include/SDL/SDL_image.h
