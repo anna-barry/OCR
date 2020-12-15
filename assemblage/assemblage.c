68ffc4591c568f9e748f48984b16121e2a957e29
mariemtrn <marie.maturana@epita.fr>
2020-12-15 05:39:54 +0100

    final ok

--- assemblage/assemblage.c
+++ assemblage/assemblage.c
@@ -19,18 +19,14 @@ void MakeOCR (char *txt, char *image)
   init_sdl();
   img = SDL_LoadBMP(image);
     
-   printf("after init \n"); 
+    
     double skew=0;
   ManualRotation(img ,skew);
-  printf("after manual rotation \n");
   Matrix matrice1 = surface_to_matrix_grayscale(img); //matrice contenant les valeurs de gris des pixels
-  printf("grayscale \n");
   int seuil = otsu(img); // calcul du seuil
   Matrix matricef =  matrix_grayscale_to_binar(matrice1, seuil) ; //matrice binaire finale
-  printf("before seg \n");
   beginSeg(matricef,txt);
-    
-  printf("after seg \n");
+
   SDL_FreeSurface(img);
 
 
