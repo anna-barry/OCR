d614ba53b0bf33024385e8ee4530063ee68d6f8d
Anna Barry <anna.barry@epita.fr>
2020-12-15 01:57:54 +0000

    added tests for rotMan

---
+++ pré-traitement/Rotations/rotationAUTO.d
@@ -0,0 +1,5 @@
+../../pré-traitement/Rotations/rotationAUTO.o: \
+ ../../pré-traitement/Rotations/rotationAUTO.c \
+ ../../pré-traitement/Rotations/../../Tools/pixel_operations.h \
+ /usr/include/SDL/SDL.h ../../pré-traitement/Rotations/rotationAUTO.h \
+ ../../pré-traitement/Rotations/../../Tools/image_operations.h
---
+++ pré-traitement/Rotations/rotationMAN.d
@@ -0,0 +1,4 @@
+../../pré-traitement/Rotations/rotationMAN.o: \
+ ../../pré-traitement/Rotations/rotationMAN.c \
+ ../../pré-traitement/Rotations/../../Tools/pixel_operations.h \
+ /usr/include/SDL/SDL.h ../../pré-traitement/Rotations/rotationMAN.h
