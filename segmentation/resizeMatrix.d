a8219f1dfca125adf9d6c7eae018fa55184fc9cd
Geoffroy du Mesnil du Buisson <geoffroy.du-mesnil-du-buisson@epita.fr>
2020-12-15 02:25:03 +0100

    NEW COMM

--- segmentation/resizeMatrix.d
+++
@@ -1,3 +0,0 @@
-../../segmentation/resizeMatrix.o: ../../segmentation/resizeMatrix.c \
-  ../../segmentation/resizeMatrix.h ../../segmentation/../Tools/matrix.h \
-  ../../segmentation/../Tools/minifunc.h
